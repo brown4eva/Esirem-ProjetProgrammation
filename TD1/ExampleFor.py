62e56368dfa04e8128c7fdefd4f1d83a7cca427c
FlorianScalvini <84083246+FlorianScalvini@users.noreply.github.com>
2022-04-06 11:07:25 +0200

    Create ExampleFor.py

---
+++ TD1/ExampleFor.py
@@ -0,0 +1,9 @@
+# Chaine de caractere :
+a = "Hello World"
+# Pour i allant de 0 a la longueur de la chaine de car
+for i in range(len(a)):
+    print(f" Le caractere { a [ i ]} est a la position { i } " )
+    i = 0
+    for c in a:
+        print(f" Le caractere { c } est a la position { c } " )
+        i = i + 1
